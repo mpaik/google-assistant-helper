d8027d313e91b5e94ca0cc626fafaaa62ffd5897
Michael Paik <michael.paik@gmail.com>
2018-10-22 19:00:20 -0400

    bugfix - clients => players

--- cast.js
+++ cast.js
@@ -151,7 +151,7 @@ function cast(serviceName, mediaUrl, mediaType, cb) {
 	client.on('error', function(err) {
 		logger.error(`Client returned error. Closing.`, err)
 	  client.close();
-	  clients[serviceName] = null;
+	  playerss[serviceName] = null;
 	});
 };
 
